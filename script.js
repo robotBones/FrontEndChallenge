fb9bda2371f2618be2fb26fb3c70764720a1526e
Kim Marquardt <kim.marquardt@gmail.com>
2017-11-04 11:56:21 -0700

    Refactor js style and handle dismissed items

--- script.js
+++ script.js
@@ -1,44 +1,49 @@
 
 function domobj(){
-  var self        =this;
+  var self        = this;
   self.products   = [];
 
-  self.getproducts = function(url){
-    return $.getJSON(url, function(response){
-        for(i=0; i<response.sales.length ; i++){
-          self.products.push( new productobj(response.sales[i], i)  );
+  self.getproducts = function(url) {
+    return $.getJSON(url, function(response) {
+        for(i=0; i<response.sales.length ; i++) {
+          self.products.push( new productobj(response.sales[i])  );
         }
     });
   }
 
-  self.updateproducthtml = function(template){
-    for( i=0; i< self.products.length ; i++){
+  self.updateproducthtml = function(template) {
+    for( i=0; i< self.products.length ; i++) {
       self.products[i].updatehtml(template);
     }
   }
 
-  self.updatedom = function(){
+  self.updatedom = function() {
     var i=0
     thishtml='';
-    for( i=0; i< self.products.length ; i++){
-      if (i % 3 == 0 ){  thishtml += "<div class='row'>"; console.log("START") }
+    for( i=0; i< self.products.length ; i++) {
+      if (i % 3 == 0 ) {
+        thishtml += "<div class='row'>";
+        console.log("START");
+      }
       thishtml += self.products[i].htmlview;
-      if ((i % 3 == 2) || i == (self.products.length-1) ){thishtml += "</div>";console.log("FINISH")}
+      if ((i % 3 == 2) || i == (self.products.length-1) ) {
+        thishtml += "</div>";
+        console.log("FINISH");
+      }
     }
     $("#content").append(thishtml)
   }
 
 }
 
-function productobj(product, i){
+function productobj(product) {
   var self          = this;
   self.photo        = product.photos.medium_half
   self.title        = product.name
   self.tagline      = product.tagline
   self.url          = product.url
   self.htmlview     = ""
-  self.index        = i
-  self.custom_class = "col"+ ((i % 3) +1)
+  // Removed custom column classes. Flex box from bootstrap aligns columns.
 
   // template is getting reused so we fetch it once instead in the updated
   // function below
@@ -53,17 +58,19 @@ function productobj(product, i){
   //   });
   // }
 
-  self.updatehtml= function(template){
+  self.updatehtml= function(template) {
     self.htmlview = template
       .replace('{image}', self.photo)
       .replace('{title}', self.title)
       .replace('{tagline}', self.tagline)
       .replace('{url}', self.url)
-      .replace('{custom_class}', self.custom_class);
+      .replace('{custom_class}', self.custom_class)
+      // I didn't see a description property
+      // .replace('{description}', self.description);
   }
 }
 
-var page=new domobj();
+var page = new domobj();
 // Use promises to chain async events. Refactored this first because
 // nothing was showing up in my browser due to race conditions
 $.when(page.getproducts('data.json'), $.get('product-template.html'))
@@ -71,4 +78,8 @@ $.when(page.getproducts('data.json'), $.get('product-template.html'))
     console.log('building html');
     page.updateproducthtml(template[0]);
     page.updatedom()
+    $('#content').on('click', '.dismiss-btn', function() {
+      event.preventDefault();
+      $(event.target).closest('.product-container').addClass('product-removed');
+    });
   });
