4fa1b66f368c4fd0c564ce9ffcb1aa55941238cf
Kim Marquardt <kim.marquardt@gmail.com>
2017-11-03 18:51:08 -0700

    Use promises to get and render products

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ function domobj(){
   self.products   = [];
 
   self.getproducts = function(url){
-    $.getJSON(url, function(response){
+    return $.getJSON(url, function(response){
         for(i=0; i<response.sales.length ; i++){
           self.products.push( new productobj(response.sales[i], i)  );
         }
     });
   }
-    
-  self.updateproducthtml = function(){
+
+  self.updateproducthtml = function(template){
     for( i=0; i< self.products.length ; i++){
-      self.products[i].updatehtml();
+      self.products[i].updatehtml(template);
     }
   }
-  
+
   self.updatedom = function(){
     var i=0
     thishtml='';
@@ -27,7 +27,7 @@ function domobj(){
     }
     $("#content").append(thishtml)
   }
-  
+
 }
 
 function productobj(product, i){
@@ -39,16 +39,36 @@ function productobj(product, i){
   self.htmlview     = ""
   self.index        = i
   self.custom_class = "col"+ ((i % 3) +1)
-  
-  self.updatehtml= function(){
-    $.get('product-template.html', function(template){
-      self.htmlview = template.replace('{image}', self.photo).replace('{title}', self.title).replace('{tagline}', self.tagline).replace('{url}', self.url).replace('{custom_class}', self.custom_class);
-    });
+
+  // template is getting reused so we fetch it once instead in the updated
+  // function below
+  // self.updatehtml= function(){
+  //   $.get('product-template.html', function(template){
+  //     self.htmlview = template
+  //       .replace('{image}', self.photo)
+  //       .replace('{title}', self.title)
+  //       .replace('{tagline}', self.tagline)
+  //       .replace('{url}', self.url)
+  //       .replace('{custom_class}', self.custom_class);
+  //   });
+  // }
+
+  self.updatehtml= function(template){
+    self.htmlview = template
+      .replace('{image}', self.photo)
+      .replace('{title}', self.title)
+      .replace('{tagline}', self.tagline)
+      .replace('{url}', self.url)
+      .replace('{custom_class}', self.custom_class);
   }
 }
 
-
 var page=new domobj();
-page.getproducts('data.json');
-setTimeout("console.log('building html');page.updateproducthtml();",20);
-setTimeout("page.updatedom()",50)
+// Use promises to chain async events. Refactored this first because
+// nothing was showing up in my browser due to race conditions
+$.when(page.getproducts('data.json'), $.get('product-template.html'))
+  .then(function(_, template) {
+    console.log('building html');
+    page.updateproducthtml(template[0]);
+    page.updatedom()
+  });
